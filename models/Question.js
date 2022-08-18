ae88ab0d42e57b4145b446196de1b79751a13890
DeveloperEducology <111419184+DeveloperEducology@users.noreply.github.com>
2022-08-18 09:18:48 +0530

    Update Question.js

--- models/Question.js
+++ models/Question.js
@@ -2,11 +2,15 @@ const mongoose = require("mongoose");
 
 const questionSchema = new mongoose.Schema({
   question: { type: String },
-  answer_options: Array,
+  options: Array,
   type: {
     type: String,
   },
   correct_option: { type: String },
+  right_answer: Array,
+  type: {
+    type: String,
+  },
   category: { type: String },
   tag: Array,
   type: {
